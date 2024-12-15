f48d8cc84389fcea98e2a06c8d85c4fde21a7ca5
Jasper <jaspermesenbrink@gmail.com>
2024-12-15 16:03:06 -0700

    add docker-compose and src folder

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.post("/submit-score")
+def parse_and_submit_score():
+    print(request)
---
+++ src/db.py
@@ -0,0 +1,8 @@
+def get_score_from_player(player: str):
+    pass
+
+def update_score(player:str, num: int):
+    pass
+
+def add_player(player:str):
+    pass
